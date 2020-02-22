7e3bc820907d26fe3ccbd7d31d1884aa717ac508
felipecapelli <felipe.ccapelli@gmail.com>
2020-02-22 18:54:49 -0300

    inclusão relatório detalhes produto

--- src/main/java/br/unip/diadefeira/controller/dto/BuscaProdutoresNumaFeiraComProdutoEspecificoDto.java
+++ src/main/java/br/unip/diadefeira/controller/dto/BuscaProdutoresNumaFeiraComProdutoEspecificoDto.java
@@ -15,14 +15,14 @@ public class BuscaProdutoresNumaFeiraComProdutoEspecificoDto implements Serializ
 	private String nomeProdutor;
 	
 	//dados da feira
-	private Long idFeira;
-	private String nomeFeira;
-	private String enderecoFeira;
+//	private Long idFeira;
+//	private String nomeFeira;
+//	private String enderecoFeira;
 	
 	//dados do Produto
-	private Long idProduto;
-	private String nomeDoProduto;
-	private Tipo tipo;
+//	private Long idProduto;
+//	private String nomeDoProduto;
+//	private Tipo tipo;
 	private boolean organico;
 	private String unidade;
 	
@@ -34,13 +34,13 @@ public class BuscaProdutoresNumaFeiraComProdutoEspecificoDto implements Serializ
 		this.email = produtorPorFeiraPorProduto.getIdChaveComposta().getProdutor().getEmail();
 		this.nomeProdutor = produtorPorFeiraPorProduto.getIdChaveComposta().getProdutor().getNome();
 		
-		this.idFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getId();
-		this.nomeFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getNome();
-		this.enderecoFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getEndereco();
+//		this.idFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getId();
+//		this.nomeFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getNome();
+//		this.enderecoFeira = produtorPorFeiraPorProduto.getIdChaveComposta().getFeira().getEndereco();
 		
-		this.idProduto = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getIdProduto();
-		this.nomeDoProduto = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getNome();
-		this.tipo = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getTipo();
+//		this.idProduto = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getIdProduto();
+//		this.nomeDoProduto = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getNome();
+//		this.tipo = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getTipo();
 		this.organico = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().isOrganico();
 		this.unidade = produtorPorFeiraPorProduto.getIdChaveComposta().getProduto().getUnidade();
 		this.preco = produtorPorFeiraPorProduto.getPreco();
@@ -62,53 +62,53 @@ public class BuscaProdutoresNumaFeiraComProdutoEspecificoDto implements Serializ
 		this.nomeProdutor = nomeProdutor;
 	}
 
-	public Long getIdFeira() {
-		return idFeira;
-	}
-
-	public void setIdFeira(Long idFeira) {
-		this.idFeira = idFeira;
-	}
-
-	public String getNomeFeira() {
-		return nomeFeira;
-	}
-
-	public void setNomeFeira(String nomeFeira) {
-		this.nomeFeira = nomeFeira;
-	}
-
-	public String getEnderecoFeira() {
-		return enderecoFeira;
-	}
-
-	public void setEnderecoFeira(String enderecoFeira) {
-		this.enderecoFeira = enderecoFeira;
-	}
-
-	public Long getIdProduto() {
-		return idProduto;
-	}
-
-	public void setIdProduto(Long idProduto) {
-		this.idProduto = idProduto;
-	}
-
-	public String getNomeDoProduto() {
-		return nomeDoProduto;
-	}
-
-	public void setNomeDoProduto(String nomeDoProduto) {
-		this.nomeDoProduto = nomeDoProduto;
-	}
-
-	public Tipo getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(Tipo tipo) {
-		this.tipo = tipo;
-	}
+//	public Long getIdFeira() {
+//		return idFeira;
+//	}
+//
+//	public void setIdFeira(Long idFeira) {
+//		this.idFeira = idFeira;
+//	}
+//
+//	public String getNomeFeira() {
+//		return nomeFeira;
+//	}
+//
+//	public void setNomeFeira(String nomeFeira) {
+//		this.nomeFeira = nomeFeira;
+//	}
+//
+//	public String getEnderecoFeira() {
+//		return enderecoFeira;
+//	}
+//
+//	public void setEnderecoFeira(String enderecoFeira) {
+//		this.enderecoFeira = enderecoFeira;
+//	}
+//
+//	public Long getIdProduto() {
+//		return idProduto;
+//	}
+//
+//	public void setIdProduto(Long idProduto) {
+//		this.idProduto = idProduto;
+//	}
+//
+//	public String getNomeDoProduto() {
+//		return nomeDoProduto;
+//	}
+//
+//	public void setNomeDoProduto(String nomeDoProduto) {
+//		this.nomeDoProduto = nomeDoProduto;
+//	}
+//
+//	public Tipo getTipo() {
+//		return tipo;
+//	}
+//
+////	public void setTipo(Tipo tipo) {
+//		this.tipo = tipo;
+//	}
 
 	public boolean isOrganico() {
 		return organico;
